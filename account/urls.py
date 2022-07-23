025e8882b1038da9291edab3307a16da28cfe746
Adewale-alt <adebimpehabeeb10@gmail.com>
2022-07-23 18:37:34 +0100

    login and logout

--- account/urls.py
+++ account/urls.py
@@ -4,4 +4,6 @@ from django.urls import path
 urlpatterns = [
     path("home/",views.home, name='home'),
     path("signup/", views.signup, name='signup'),
+    path("login/", views.loginpage, name='login'),
+    path('logout', views.userlogout, name='logout'),
 ]
--- auth_wiki/settings.py
+++ auth_wiki/settings.py
@@ -13,8 +13,8 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 from pathlib import Path
 
 import os
-from dotenv import load_dotenv
-load_dotenv()
+# from dotenv import load_dotenv
+# load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -24,7 +24,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ["SECRET_KEY"]
+# SECRET_KEY = os.environ["SECRET_KEY"]
+SECRET_KEY = 'django-insecure-9&bbf(+np&$eirxezv2xapvzd2si-vj6_%dxw9)q4+^e_4pdhj'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
